e04e689f9bb7c0372a9cedece5d643692c741c7f
Enver <enver027@yandex.ru>
2023-07-06 07:42:45 +0300

    обьединил файлы js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
+const profileName = document.querySelector(".profile__name");
+const profileWork = document.querySelector(".profile__work");
+const profileCreateButton = document.querySelector(".profile__create");
+const profilePopup = document.querySelector(".popup_edit-profile");
+const profileEditForm = profilePopup.querySelector(".popup__form");
+const profileInputName = profilePopup.querySelector(".popup__input_value_name");
+const profileInputWork = profilePopup.querySelector(".popup__input_value_job");
+const popupEditCloseButton = profilePopup.querySelector(".popup__close");
 const newAddCardButton = document.querySelector(".profile__add");
 const cardPopup = document.querySelector(".popup_add");
 const newCardForm = cardPopup.querySelector(".popup__form");
@@ -10,6 +18,34 @@ const cardTemplate = document.querySelector("#card").content;
 const cardsContainer = document.querySelector(".cards");
 const cardAddCloseButton = cardPopup.querySelector(".popup__close");
 const photoCloseButton = imagePopup.querySelector(".popup__close");
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
+
+function saveProfile(event) {
+  event.preventDefault();
+  profileName.textContent = profileInputName.value;
+  profileWork.textContent = profileInputWork.value;
+  closePopup(profilePopup);
+}
+function openProfilePopup() {
+  profileInputName.value = profileName.textContent;
+  profileInputWork.value = profileWork.textContent;
+  openPopup(profilePopup);
+}
+
+function closeProfilePopup() {
+  closePopup(profilePopup);
+}
+
+profileCreateButton.addEventListener("click", openProfilePopup);
+profileEditForm.addEventListener("submit", saveProfile);
+popupEditCloseButton.addEventListener("click", closeProfilePopup);
 
 function saveCardNew(event) {
   event.preventDefault();
